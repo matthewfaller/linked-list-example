69f3321e1bde2b62a90a9b7bfdf1af831783b0fb
Matthew Faller <matthew.faller@rbc.com>
2020-12-31 17:39:59 -0500

    Added skeletal structure for "friendly" linked list.
    Please fill this out using recursion.

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,27 +1,52 @@
+import com.sun.istack.internal.Nullable;
+
 /**
  * Created by faller.matthew on 12/30/20.
  */
-public class LinkedList {
+public class LinkedList /*implements Iterable<Integer>*/ {
 
-git
+    // A private piece of data that hides away the uglyness of the data structure from our users aka the programmers
+    @Nullable
+    private Node zeroethNode;
 
-    public void addData(int newData) {
+    /**
+     * Appends a new element to the end of the list
+     * */
+    public void appendData(int newData) {
 
     }
 
+    /**
+     * Returns the current number of items in the list
+     * */
+    public int getCount() {
+        return 0;
+    }
+
+    /**
+     * Removes the first element from the list and returns the data value
+     * */
     public int removeFirst() {
         return 0;
     }
 
+    /**
+     * Removes the last element from the list and returns the data value
+     * */
     public int removeLast() {
         return 0;
     }
 
+    /**
+     * Removes all elements
+     * */
     public void removeAll() {
 
     }
 
-
+    /**
+     * Adds all of the elements from the linked list to this list
+     * */
     public void addData(LinkedList list) {
 
     }
